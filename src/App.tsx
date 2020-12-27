da642b4d936e5cf7c7db48672531de5a1f0e87b3
YSRKEN <ysr.ken@gmail.com>
2020-12-27 22:48:36 +0900

    [add] 他の入力値も入力できるようにして、入力結果を可視化することも行った

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,8 @@ import React, { createContext, useContext, useEffect, useState } from 'react';
 import { Col, Container, Form, Row } from 'react-bootstrap';
 
 type SensorSize = 'full' | 'apsc-c' | 'apsc-x' | 'mft';
-type ActionType = 'setSensorSize';
+type TrailType = 'pin-point' | 'slight' | 'visible';
+type ActionType = 'setSensorSize' | 'setPixelWidth' | 'setFocalLength' | 'setFNumber10' | 'setTrailType';
 
 interface Action {
   type: ActionType;
@@ -11,24 +12,56 @@ interface Action {
 
 interface AppStore {
   sensorSize: SensorSize;
+  pixelWidth: number;
+  focalLength: number;
+  fNumber10: number;
+  trailType: TrailType;
   dispatch: (action: Action) => void;
 }
 
 const useAppStore = (): AppStore => {
   const [sensorSize, setSensorSize] = useState<SensorSize>('full');
+  const [pixelWidth, setPixelWidth] = useState(6000);
+  const [focalLength, setFocalLength] = useState(50);
+  const [fNumber10, setFNumber10] = useState(14);
+  const [trailType, setTrailType] = useState<TrailType>('pin-point');
 
-  useEffect(() => { console.log(`sensorSize=${sensorSize}`) }, [sensorSize]);
+  useEffect(() => {
+    console.log({
+      sensorSize,
+      pixelWidth,
+      focalLength,
+      fNumber10,
+      trailType
+    })
+  }, [sensorSize, pixelWidth, focalLength, fNumber10, trailType]);
 
   const dispatch = (action: Action) => {
     switch (action.type) {
       case 'setSensorSize':
         setSensorSize(action.message as SensorSize);
         break;
+      case 'setPixelWidth':
+        setPixelWidth(parseInt(action.message as string, 10));
+        break;
+      case 'setFocalLength':
+        setFocalLength(parseInt(action.message as string, 10));
+        break;
+      case 'setFNumber10':
+        setFNumber10(parseInt(action.message as string, 10));
+        break;
+      case 'setTrailType':
+        setTrailType(action.message as TrailType);
+        break;
     }
   };
 
   return {
     sensorSize,
+    pixelWidth,
+    focalLength,
+    fNumber10,
+    trailType,
     dispatch
   };
 };
@@ -36,7 +69,7 @@ const useAppStore = (): AppStore => {
 const AppContext = createContext<AppStore>({} as AppStore);
 
 const InputForm: React.FC = () => {
-  const { sensorSize, dispatch } = useContext(AppContext);
+  const { sensorSize, pixelWidth, focalLength, fNumber10, trailType, dispatch } = useContext(AppContext);
 
   return <Form className="border p-3">
     <Form.Group>
@@ -53,27 +86,31 @@ const InputForm: React.FC = () => {
     </Form.Group>
     <Form.Group>
       <Form.Label>
-        画像の横幅のピクセル数
-    </Form.Label>
-      <Form.Control type="range" min={1} max={10000} />
+        画像の横幅： {pixelWidth} [ピクセル]
+      </Form.Label>
+      <Form.Control type="range" min={1} max={10000} value={pixelWidth}
+        onChange={(e) => dispatch({ type: 'setPixelWidth', message: e.currentTarget.value })} />
     </Form.Group>
     <Form.Group>
       <Form.Label>
-        実焦点距離
+        実焦点距離： {focalLength} [mm]
     </Form.Label>
-      <Form.Control type="range" min={1} max={1000} />
+      <Form.Control type="range" min={1} max={1000} value={focalLength}
+        onChange={(e) => dispatch({ type: 'setFocalLength', message: e.currentTarget.value })} />
     </Form.Group>
     <Form.Group>
       <Form.Label>
-        絞り値
-    </Form.Label>
-      <Form.Control type="range" min={1} max={36} />
+        絞り値： F{1.0 * fNumber10 / 10}
+      </Form.Label>
+      <Form.Control type="range" min={7} max={360} value={fNumber10}
+        onChange={(e) => dispatch({ type: 'setFNumber10', message: e.currentTarget.value })} />
     </Form.Group>
     <Form.Group>
       <Form.Label>
         星の写り方
     </Form.Label>
-      <Form.Control as="select">
+      <Form.Control as="select" value={trailType}
+        onChange={(e) => dispatch({ type: 'setTrailType', message: e.currentTarget.value })}>
         <option value="pin-point">一つの点</option>
         <option value="slight">Slight Trail</option>
         <option value="visible">Visible Trail</option>
